7f3de1f338611f30cae250d6194b9a3c9e979a1b
srinivasarynh <srinivasynh@gmail.com>
2024-12-17 10:54:01 +0530

    localhost connection issue fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = require('./app');
 
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+const server = app.listen(port, '0.0.0.0',  () => {
   console.log(`App running on port ${port}...`);
 });
 
